e492cb133e38ad92f94e4ebb96dae00162a895df
justs <s0930249@sms.ed.ac.uk>
2011-06-21 17:07:10 +0100

    Ancestor processing added, but it's slow. Maybe it's supposed to be that way.

--- src/groovy/webtax/Blaster.groovy
+++ src/groovy/webtax/Blaster.groovy
@@ -1,13 +1,16 @@
+//Most of this file is a port of Martin Jones' Taxonerator.
+
 package webtax
 
 class Blaster {
-	
+
 	def blastDatabase = "/home/justs/workspace/WebTax/databases/silva/ssu_silva.fasta"
 	//def megablastPath = "/home/justs/Desktop/blast/megablast"
 	def megablastPath = "/usr/bin/megablast"
-	
+	def taxdumpPath = '/home/justs/workspace/WebTax/databases/NCBIdump'
+
 	def processors = 1
-	
+
 	def motuID
 	def seq
 
@@ -17,7 +20,7 @@ class Blaster {
 	def void doBlast(Motu inputMotu) {
 		motuID = inputMotu.seqID
 		seq = inputMotu.sequence
-		
+
 		File blastInput = new File('blastInput.fsa')
 		blastInput.delete()
 		blastInput.append(">$motuID\n$seq\n")
@@ -30,11 +33,11 @@ class Blaster {
 		}
 
 		//call blast
-		def command = "$megablastPath -d $blastDatabase -i blastInput.fsa -a $processors -m 8 -v 10 -b 10 -H 1"
+		def command = "$megablastPath -d $blastDatabase -i blastInput.fsa -a $processors -m 8 -v 1 -b 1 -H 1"
 		Process proc = command.execute()
 		proc.waitFor()
 
-		//Map results = [:]
+
 
 		//process blast output
 		proc.in.eachLine{ line ->
@@ -42,21 +45,91 @@ class Blaster {
 			def motu = rows[0]
 			def hit = rows[1]
 			Integer score = rows[11].toFloat() //Changed from float to int. Can the score even be a float?
+
+			def taxon = getTaxidForHit(hit)
 			
-			def aHit = new BlastHit(accNum: hit, bitScore: score)
+
+			def aHit = new BlastHit(accNum: hit, bitScore: score, taxID: taxon)
+			addLineage(taxon, aHit)
+
+
+
 			//println hit
 			//println score
 			Motu.get(inputMotu.id).addToHits(aHit)
-			//results.put(hit, score)
+
 		}
 
-		//seq2results.put(it.seq, results)
-	}
-
-	//seq2results.get(it.seq).each{ entry ->
-		//sql.execute("""insert into blast_hit (motu_id, acc, bitscore) values ($it.motu_id, $entry.key, $entry.value)""")
 	}
 
 
+	//Not checking if the taxID has been already accessed before.
+	Integer getTaxidForHit(String acc){
+		String result
+		// use try catch because ncbi eutils are unreliable
+		try{
+			URL efetch = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=$acc&rettype=gb".toURL()
+			String gb = efetch.text
+
+			gb.eachLine({
+				if (it.startsWith('                     /db_xref="taxon:')){
+					def taxidMatcher = (it =~ /taxon:(\d+)/)
+					result = taxidMatcher[0][1]
+				}
+			})
+		} catch(e){
+			result = -1
+		}
+
+		if (result == null){
+			return -1
+		}
+		else{
+			return result.toInteger()
+		}
 
 
+	}
+	
+	void addLineage(Integer taxid, BlastHit hit) {		//might be problems if the databese is not accessed and only the BlastHit modified
+		TaxIdProcessor myTreeData = new TaxIdProcessor(taxdumpPath)
+		
+				println 'making node'
+				def node = myTreeData.getNodeForTaxid(taxid)
+				println 'node made'
+		
+				for (TreeNode ancestor in myTreeData.getAncestorsForNode(node)){
+					if (ancestor.rank == 'species') {
+						hit.species = ancestor.name
+					} else if (ancestor.rank == 'genus') {
+						hit.genus = ancestor.name
+					} else if (ancestor.rank == 'order') {
+						hit.taxOrder = ancestor.name
+					} else if (ancestor.rank == 'family') {
+						hit.family = ancestor.name
+					} else if (ancestor.rank == 'class') {
+						hit.taxClass = ancestor.name
+					} else if (ancestor.rank == 'phylum') {
+						hit.phylum = ancestor.name
+					} 
+					
+					
+					
+					// only add this tree node if we haven't already done so
+		
+					//println "adding info for $ancestor.taxid to database"
+					println "$ancestor.name, $ancestor.rank, $ancestor.taxid"
+					
+					//taxid needed?
+					
+					
+					// keep a record of the taxid so we don't add it next time
+					//taxAdded.add(ancestor.taxid)
+		
+		
+				}
+	}
+
+
+
+}
---
+++ src/groovy/webtax/TaxIdProcessor.groovy
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webtax
+
+import java.util.regex.Pattern
+/**
+ *
+ * @author martin
+ */
+class TaxIdProcessor {
+
+    HashMap<Integer, TreeNode> taxid2node = [:]
+    HashMap<Integer, Integer> child2parent = [:]
+
+
+    def TaxIdProcessor(taxdumpPath){
+
+
+        def nodePattern = ~/^(\d+)\t\|\t(\d+)\t\|\t(.+?)\t\|/
+
+        println "reading ncbi taxonomy from $taxdumpPath"
+        def count=0
+        // open the NCBI taxonomy for structure
+        new File("${taxdumpPath}/nodes.dmp").eachLine{
+            line ->
+            count++
+            def matcher = (line =~ nodePattern)
+            if (matcher.matches()){
+                Integer myId = matcher[0][1].toInteger()
+                Integer parentId = matcher[0][2].toInteger()
+                String myRank = matcher[0][3]
+
+                // println "$myId is child of $parentId and has rank $myRank"
+                if ((count % 1000) ==0){
+                    print "processing node $count\n"
+                }
+                // build new node
+                def node = new TreeNode(name : "my id is $myId", taxid : myId, rank:myRank)
+                // add node to lookup hash
+                taxid2node[(myId)] = node
+                // add to relationship hash
+                // we can't just add to parent directly because the nodes are not
+                // in any kind of helpful order
+
+                child2parent[(myId)] = parentId
+
+
+            }
+        }
+        // now process names file to add scientific names to nodes
+        Pattern namePattern = ~/^(\d+)\t\|\t(.+)\t\|\t(.*)\t\|\t(.+)\t\|/
+        new File("${taxdumpPath}/names.dmp").eachLine{
+            line ->
+            def nameMatcher = (line =~ namePattern)
+
+
+            if (nameMatcher.matches() && nameMatcher[0][4].equals("scientific name") ){
+                Integer taxid = nameMatcher[0][1].toInteger()
+                String myName = nameMatcher[0][2]
+
+                //println "scientific name is $name"
+                taxid2node.get(taxid).name = myName
+            }
+        }
+
+        println "finished processing relationships"
+
+    }
+
+    List<TreeNode> getAncestorsForNode(TreeNode n){
+        //println "getting ancestors for " + n.taxid
+        List<TreeNode> result = []
+        TreeNode current = n
+        while (current.taxid != 1){
+            //println "adding $current.name"
+            result.add(current)
+            def currentTaxid = child2parent.get(current.taxid)
+            def parent = taxid2node.get(currentTaxid)
+            current = parent
+         //   println "\tcurrent is $current.taxid"
+        }
+        return result
+    }
+
+
+    TreeNode getNodeForTaxid(Integer taxid){
+        return this.taxid2node.get(taxid)
+    }
+
+
+}
+
+// start NCBI stuff
+
+
+class TreeNode {
+    String name
+    String rank
+    Integer taxid
+}
